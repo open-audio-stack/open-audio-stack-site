2f5f5bbdc294a4933f9513420931830cedc07c99
Kim T <kimturley@gmail.com>
2025-06-07 22:41:51 +0200

    Fix asset bath path

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from 'next';
 console.log('NODE_ENV', process.env.NODE_ENV);
 console.log('CI', process.env.CI);
 
+const basePath = process.env.CI ? '/open-audio-stack-site' : '';
+
 const nextConfig: NextConfig = {
-  basePath: process.env.CI ? '/open-audio-stack-site' : '',
+  basePath,
+  assetPrefix: basePath,
   generateBuildId: async () => {
     return 'latest'; // TODO version the site feed using package.version
   },
