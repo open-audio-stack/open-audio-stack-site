e9466af4aa0ef6466ffc2cbcd2fdaee82e1cb926
Adam <134429563+RustoMCSpit@users.noreply.github.com>
2025-07-20 13:24:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,32 @@ export default function Home() {
         <main className={styles.mainColumns}>
           <div className={styles.card}></div>
           <p className={styles.message}>
-          Open Audio Stack is a newly created set of open-source audio package management software and standards. The goal is to simplify the distribution and installation of FOSS plugins, presets, projects, and eventually also samples and MIDI.
+  Open Audio Stack is a newly created set of open-source audio package management software and standards. The 
 
-Platforms that have currently integrated OAS as of July 2025:
-- [StudioRack](https://studiorack.github.io/studiorack-site)
-- [OwlPlug](https://owlplug.com)
-
-The registry API can be accessed [here](https://open-audio-stack.github.io/open-audio-stack-registry)
-
-The npm package can be accessed [here](https://www.npmjs.com/package/@open-audio-stack/core)
-
-For any inquiries, [get in touch](hello@kimturley.co.uk)
-
-Our Github is [here](https://github.com/open-audio-stack)
-        </p>
+  goal is to simplify the distribution and installation of FOSS plugins, presets, projects, and eventually 
+also 
+  samples and MIDI.
+          </p>
+          <p className={styles.message}>
+  Platforms that have currently integrated OAS as of July 2025:
+  <br/>
+  <a href="https://studiorack.github.io/studiorack-site">StudioRack</a>
+  <br/>
+  <a href="https://owlplug.com">OwlPlug</a>
+          </p>
+          <p className={styles.message}>
+  The registry API can be accessed <a 
+href="https://open-audio-stack.github.io/open-audio-stack-registry">here</a>
+          </p>
+          <p className={styles.message}>
+  The npm package can be accessed <a href="https://www.npmjs.com/package/@open-audio-stack/core">here</a>
+          </p>
+          <p className={styles.message}>
+  For any inquiries, <a href="mailto:hello@kimturley.co.uk">get in touch</a>
+          </p>
+          <p className={styles.message}>
+  Our Github is <a href="https://github.com/open-audio-stack">here</a>
+          </p>
         </main>
       </section>
     </div>
